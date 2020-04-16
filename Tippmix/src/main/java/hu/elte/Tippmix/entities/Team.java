950516050efbd14aefacac743e6a3a8711dcd2bb
tlaszlo <takacslaczy@gmail.com>
2020-04-16 21:05:03 +0200

    Team done
    The base of Team is done.

--- Tippmix/src/main/java/hu/elte/Tippmix/entities/Team.java
+++ Tippmix/src/main/java/hu/elte/Tippmix/entities/Team.java
@@ -6,11 +6,21 @@
 package hu.elte.Tippmix.entities;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 public class Team {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,21 +30,4 @@ public class Team {
     @NotNull
     private String name;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-    
-    
 }
--- Tippmix/src/main/resources/import.sql
+++ Tippmix/src/main/resources/import.sql
@@ -8,8 +8,8 @@
  * Created: 2020.04.16.
  */
 
-insert into team(team_id, name) values (1, 'Fradi');
-insert into team(team_id, name) values(2, 'Újpest');
+insert into team(id, name) values (1, 'Fradi');
+insert into team(id, name) values(2, 'Újpest');
 
 insert into user (username, password, enabled, role) values ('admin', '$2a$04$YDiv9c./ytEGZQopFfExoOgGlJL6/o0er0K.hiGb5TGKHUL8Ebn..', true, 'ROLE_ADMIN');
 insert into user (username, password, enabled, role) values ('user', '$2a$04$YDiv9c./ytEGZQopFfExoOgGlJL6/o0er0K.hiGb5TGKHUL8Ebn..', true, 'ROLE_USER'); 
