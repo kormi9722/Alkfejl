64692cefea0e8a9a5ca86bec01328b902630b7ca
tlaszlo <takacslaczy@gmail.com>
2020-04-16 20:42:24 +0200

    Focus on Team

---
+++ Tippmix/src/main/resources/import.sql
@@ -0,0 +1,15 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  w10_3
+ * Created: 2020.04.16.
+ */
+
+insert into team(team_id, name) values (1, 'Fradi');
+insert into team(team_id, name) values(2, 'Újpest');
+
+insert into user (username, password, enabled, role) values ('admin', '$2a$04$YDiv9c./ytEGZQopFfExoOgGlJL6/o0er0K.hiGb5TGKHUL8Ebn..', true, 'ROLE_ADMIN');
+insert into user (username, password, enabled, role) values ('user', '$2a$04$YDiv9c./ytEGZQopFfExoOgGlJL6/o0er0K.hiGb5TGKHUL8Ebn..', true, 'ROLE_USER'); 
