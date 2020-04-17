cfc9a1aa5d047e1a69108f75afb8338c109fd00c
kormi9722 <62106123+kormi9722@users.noreply.github.com>
2020-04-17 02:30:39 +0200

    Group Entity Created

---
+++ Tippmix/src/main/java/hu/elte/Tippmix/entities/Group.java
@@ -0,0 +1,39 @@
+package hu.elte.Tippmix.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import java.util.List;
+import java.io.Serializable;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+
+public class Group implements Serializable{
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+    @Column(unique = true)
+    @NotNull
+    private String name;
+    
+    @Column
+    @ManyToMany
+    @JoinTable
+    private List<User> users;
+
+}
