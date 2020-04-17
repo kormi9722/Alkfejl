8e60d34c4d8efbdd1e1267b1e667ceedf5f768b2
kormi9722 <62106123+kormi9722@users.noreply.github.com>
2020-04-17 12:39:45 +0200

    Team Build Success

--- Tippmix/src/main/java/hu/elte/Tippmix/entities/Team.java
+++ Tippmix/src/main/java/hu/elte/Tippmix/entities/Team.java
@@ -1,10 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hu.elte.Tippmix.entities;
 
+import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,22 +8,23 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.Data;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
-public class Team {
+
+public class Team implements Serializable {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     
-    @Column(unique = true)
+    @Column(name = "NAME")
     @NotNull
     private String name;
-
 }
