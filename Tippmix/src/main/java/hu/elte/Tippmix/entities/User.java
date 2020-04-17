db550267dc9d0df604f5422469c6edbe1bb324fd
kormi9722 <62106123+kormi9722@users.noreply.github.com>
2020-04-17 12:41:27 +0200

    To be modified

---
+++ Tippmix/src/main/java/hu/elte/Tippmix/entities/User.java
@@ -0,0 +1,47 @@
+package hu.elte.Tippmix.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+
+public class User implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+    @Column(unique = true)
+    @NotNull
+    private String name;
+    
+    @Column(name = "POINTS")
+    private Integer points;
+    
+    @JsonIgnore
+    @OneToMany(mappedBy = "user")
+    private List<Ticket> ticket;
+    
+    @JsonIgnore
+    @ManyToMany(mappedBy = "user")
+    private List<Group> group;
+    
+    
+}
