ceef8e8dee291501ea4351f913bfce6275095fd0
kormi9722 <62106123+kormi9722@users.noreply.github.com>
2020-04-17 02:31:30 +0200

    Group Repository Created

---
+++ Tippmix/src/main/java/hu/elte/Tippmix/repositories/GroupRepository.java
@@ -0,0 +1,8 @@
+package hu.elte.Tippmix.repositories;
+
+import hu.elte.Tippmix.entities.Group;
+import org.springframework.data.repository.CrudRepository;
+
+public interface GroupRepository extends CrudRepository<Group, Integer>{
+
+}
