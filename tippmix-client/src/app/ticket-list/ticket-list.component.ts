d75df99af8e6e430d2a1acf77187105f24d501d3
tlaszlo <takacslaczy@gmail.com>
2020-05-28 23:08:23 +0200

    Tickets in list
    Tickets added in the list. List-component can list them due to the
    ngFor.

--- tippmix-client/src/app/ticket-list/ticket-list.component.ts
+++ tippmix-client/src/app/ticket-list/ticket-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ticket } from '../ticket';
 
 @Component({
   selector: 'ticket-list',
@@ -6,6 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ticket-list.component.css']
 })
 export class TicketListComponent implements OnInit {
+
+  tickets: Ticket[] =
+    [
+      {
+        id: 1,
+        fixture: 300,
+        bets: 'Good',
+      },
+      {
+        id: 2,
+        fixture: 30,
+        bets: 'Middle',
+      },
+      {
+        id: 3,
+        fixture: -30,
+        bets: 'Bad',
+      },
+    ]
 
   constructor() { }
 
