4da1b50377a1e9194c1bd49fba53c5bdbfd9d255
tlaszlo <takacslaczy@gmail.com>
2020-05-28 12:57:34 +0200

    import.sql modified
    Data in good order now

--- Tippmix/src/main/resources/import.sql
+++ Tippmix/src/main/resources/import.sql
@@ -1,19 +1,17 @@
 insert into team(name) values ('Fradi');
 insert into team(name) values('Újpest');
 
-insert into ticket(fixture, bets, user_id) values(1,30, 'Good');
-insert into ticket(fixture, bets, user_id ) values(2,300, 'Middle');
-insert into ticket(fixture, bets, user_id) values(3,-30, 'Bad');
-
-
 insert into league(name) values('Indigo');
 insert into league(name) values('Orange');
-
-insert into league_user(leagues_id, user_id) values(1,1);
-insert into league_user(leagues_id, user_id) values(2,2);
-insert into league_user(leagues_id, user_id) values(1,3);
-
 
 insert into user (name, username, password, enabled, role, points) values ('Sanyi', 'admin', '$2a$04$YDiv9c./ytEGZQopFfExoOgGlJL6/o0er0K.hiGb5TGKHUL8Ebn..', true, 'ROLE_ADMIN', 0);
 insert into user (name, username, password, enabled, role, points) values ('Karcsi','user1', '$2a$04$YDiv9c./ytEGZQopFfExoOgGlJL6/o0er0K.hiGb5TGKHUL8Ebn..', true, 'ROLE_USER', 10); 
 insert into user (name, username, password, enabled, role, points) values ('Aranka','user2', '$2a$04$YDiv9c./ytEGZQopFfExoOgGlJL6/o0er0K.hiGb5TGKHUL8Ebn..', true, 'ROLE_USER', 30); 
+
+insert into ticket(user_id, fixture, bets) values(1,30, 'Good');
+insert into ticket(user_id, fixture, bets) values(2,300, 'Middle');
+insert into ticket(user_id, fixture, bets) values(3,-30, 'Bad');
+
+insert into league_user(leagues_id, user_id) values(1,1);
+insert into league_user(leagues_id, user_id) values(2,2);
+insert into league_user(leagues_id, user_id) values(1,3);
